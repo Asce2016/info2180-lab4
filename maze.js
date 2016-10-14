d36c0365531ff60586ec27bb2035890a967c7d26
Asce2016 <asce2016@gmail.com>
2016-10-14 12:10:02 +0000

    part 2

--- maze.js
+++ maze.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 
 
-  
+
   var boundaryone = document.getElementById("boundary1");
    boundaryone.addEventListener("mouseover",changecolour);
 
@@ -9,4 +9,27 @@ window.onload = function(){
     function changecolour(){
       document.getElementById("boundary1").setAttribute ("class","boundary youlose");
     }
+
+
+
+
+
+
+
+var boundaries = document.querySelectorAll(".boundary");
+  for (var i = 0; i < boundaries.length; i++){
+  boundaries[i].onmouseover = overBoundary;
+
+}
+
+
+function overBoundary(){
+  var boundaries = document.querySelectorAll(".boundary");
+  for (var i = 0; i < boundaries.length; i++){
+    boundaries[i].setAttribute("class", "boundary youlose");
+  }
+
+}
+
+
 }
